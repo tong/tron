217cdb9a658ee888159977ad30fb3473762fe793
tong <tong@disktree.net>
2020-06-06 19:47:52 +0200

    Optional parent object

--- Sources/tron/SpawnTools.hx
+++ Sources/tron/SpawnTools.hx
@@ -9,31 +9,31 @@ class SpawnTools {
 	}
 	*/
 
-	public static inline function spawnObjectFromScene( sceneName : String, name: String, parent: Object, done: Object->Void, ?spawnChildren : Bool ) {
+	/**
+	 * Spawn object tree from another scene
+	 */
+	public static inline function spawnObjectFromScene( sceneName : String, name : String, ?parent : Object, done : Object->Void, ?spawnChildren : Bool ) {
 		Data.getSceneRaw( sceneName, raw -> spawnObject( raw, name, parent, done, spawnChildren ) );
 	}
 	
-	// custom method to spawn object tree from another scene
-	public static function spawnObject( ?raw : TSceneFormat, name: String, parent: Object, done: Object->Void, spawnChildren = true ) {
+	public static function spawnObject( ?raw : TSceneFormat, name : String, ?parent : Object, done : Object->Void, spawnChildren = true ) {
 		if( raw == null ) raw = Scene.active.raw;
 		var objectsTraversed = 0;
 		var obj = Scene.getRawObjectByName( raw, name );
 		var objectsCount = spawnChildren ? getObjectsCount([obj], false) : 1;
 		function spawnObjectTree(obj: TObj, parent: Object, parentObject: TObj, done: Object->Void) {
-			Scene.active.createObject(obj, raw, parent, parentObject, function(object: Object) {
-				if (spawnChildren && obj.children != null) {
-					for (child in obj.children) spawnObjectTree(child, object, obj, done);
+			Scene.active.createObject( obj, raw, parent, parentObject, function(object: Object) {
+				if( spawnChildren && obj.children != null ) {
+					for( c in obj.children ) spawnObjectTree( c, object, obj, done );
 				}
-				if (++objectsTraversed == objectsCount && done != null) {
-					// Retrieve the originally spawned object from the current
-					// child object to ensure done() is called with the right
-					// object
-					while (object.name != name) object = object.parent;
-					done(object);
+				if( ++objectsTraversed == objectsCount && done != null ) {
+					// Retrieve the originally spawned object from the current child object to ensure done() is called with the right object
+					while( object.name != name ) object = object.parent;
+					done( object );
 				}
 			});
 		}
-		spawnObjectTree(obj, parent, null, done);
+		spawnObjectTree( obj, parent, null, done );
 	}
 
 	static function getObjectsCount( objects : Array<TObj>, discardNoSpawn = true ) : Int {
