5033641f00403fe403779a96a5bfad98fddcb4e9
tong <tong@disktree.net>
2020-06-08 19:31:52 +0200

    Add music player wrapper properties

--- Sources/tron/Music.hx
+++ Sources/tron/Music.hx
@@ -6,11 +6,26 @@ class Music {
 
 	public static var track(default,null) : String;
 	public static var paused(default,null) = false;
-
+	
 	public static var volume(default,set) = 1.0;
 	static inline function set_volume(v:Float) {
 		if( channel != null ) channel.volume = v;
 		return volume = v;
+	}
+
+	public static var finished(get,never) : Bool;
+	static inline function get_finished() {
+		return (channel == null) ? false : channel.finished;
+	}
+
+	public static var length(get,never) : Float;
+	static inline function get_length() {
+		return (channel == null) ? null : channel.length;
+	}
+
+	public static var position(get,never) : Float;
+	static inline function get_position() {
+		return (channel == null) ? null : channel.position;
 	}
 
 	static var channel : AudioChannel;
