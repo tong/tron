019ab5880b9654d8a0e784da2d52226d055797e4
tong <tong@disktree.net>
2020-07-03 13:56:16 +0200

    Fix

--- Sources/tron/DataTools.hx
+++ Sources/tron/DataTools.hx
@@ -7,7 +7,7 @@ class DataTools {
 	public static function loadImages( names : Array<String>, ?prefix : String, ?extension : String, done : Array<kha.Image>->Void ) {
 		var loaded = new Array<kha.Image>();
 		function loadNext() {
-			var name = names[images.length];
+			var name = names[loaded.length];
 			if( prefix != null ) name = prefix + name;
 			if( extension != null ) name += '.'+extension;
 			Data.getImage( name, img -> {
