6d9a270b1c035f630ba8791a0622be5408595beb
tong <tong@disktree.net>
2021-08-15 01:29:29 +0200

    …

--- khafile.js
+++ khafile.js
@@ -1,6 +1,6 @@
-console.info("T   R   ⚫   Ͷ");
-var project = new Project('Tron');
-project.addDefine('tron');
+console.info("T   R   ⚫  Ͷ");
+var project = new Project('tron');
+// project.addDefine('tron');
 //project.addAssets('Assets/font/**', { notinlist: false });
 //project.addShaders('Shaders/**');
 project.addSources('Sources');
