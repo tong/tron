fb129618a240d65baee7aafbbb7004f319bae5d6
tong <tong@disktree.net>
2021-08-27 22:29:13 +0200

    Add docs build file

--- khafile.js
+++ khafile.js
@@ -1,14 +1,12 @@
-// const cp = require('child_process');
-process.stdout.write("   |  |  |   T   R   ⚫  Ͷ\n");
+process.stdout.write("   T   R   ⚫  Ͷ\n");
 
+// const cp = require('child_process');
 const fs = require('fs');
 
 let project = new Project('tron');
-
 project.addSources('Sources');
 //project.addShaders('Shaders/**');
 //project.addAssets('Assets/font/**', { notinlist: false });
-
 // var cwd = process.cwd();
-
+// project.addParameter('--macro tron.Build.application()');
 resolve(project);
