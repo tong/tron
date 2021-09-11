a9520ca52eabf1207efe0901265746d9fac65a0d
tong <tong@disktree.net>
2021-09-11 15:01:05 +0200

    …

--- Sources/tron/log/Level.hx
+++
@@ -1,9 +0,0 @@
-package tron.log;
-
-enum abstract Level(Int) from Int to Int {
-	var Log = 0;
-	var Debug = 1;
-	var Info = 2;
-	var Warn = 3;
-	var Error = 4;
-}
--- khafile.js
+++ khafile.js
@@ -1,15 +1,14 @@
-process.stdout.write("   T   R   ⚫  Ͷ\n");
+process.stdout.write("T   R   ⚫  Ͷ\n");
 
 // const cp = require('child_process');
 // const fs = require('fs');
 // var cwd = process.cwd();
 
 let project = new Project('tron');
-
 project.addSources('Sources');
-project.addParameter('-main tron.App');
-//project.addShaders('Shaders/**');
+// project.addParameter('-main tron.App');
+// project.addDefine("source-header=TR0N");
+// project.addShaders('Shaders/**');
 //project.addAssets('Assets/font/**', { notinlist: false });
 // project.addParameter('--macro tron.Build.application()');
-
 resolve(project);
