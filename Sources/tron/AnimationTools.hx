ec4b366fa531e84d392d2a191fb7561b50337d9d
tong <tong@disktree.net>
2021-10-23 20:25:16 +0200

    Cleanup

--- Sources/tron/AnimationTools.hx
+++ Sources/tron/AnimationTools.hx
@@ -5,20 +5,20 @@ import iron.object.Object;
 
 class AnimationTools {
 
-	public static function findAnimation( obj : Object ) : BoneAnimation {
+	public static function findBoneAnimation( obj : Object ) : BoneAnimation {
 		if( obj.animation != null )
 			return cast obj.animation;
 		for( c in obj.children ) {
-			var co = findAnimation( c );
-			if( co != null ) return co;
+			var a = findBoneAnimation( c );
+			if( a != null ) return a;
 		}
 		return null;
 	}
 
 	public static function getAnimation( obj : Object ) : BoneAnimation {
-		var anim = obj.animation;
-		if( anim == null ) anim = obj.children[0].animation;
-		return anim;
+		var a = obj.animation;
+		if( a == null ) a = obj.children[0].animation;
+		return cast a;
 	}
 
 }
